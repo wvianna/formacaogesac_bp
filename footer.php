eaf1483f27a15d34030bdb5d156834fa16c703d1
root <root@gesac.ifce.edu.br>
2010-08-18 11:32:08 -0300

    Corrigindo tema

--- footer.php
+++ footer.php
@@ -103,22 +103,22 @@
     <div class="middle">
         <ul class="menu">
             <li><a href="#">Início</a></li>
-            <li class=""><a href="http://culturadigital.br/faqs/" title="FAQs">FAQs</a></li>
-            <li class=""><a href="http://culturadigital.br/termos-de-uso/" title="Termos de uso">Termos de uso</a></li>
-            <li class=""><a href="http://culturadigital.br/quem-faz/" title="Quem Faz">Quem Faz</a></li>
+            <li class=""><a href="<?php echo site_url() ?>/faqs/" title="FAQs">FAQs</a></li>
+            <li class=""><a href="<?php echo site_url() ?>/termo-de-uso/" title="Termos de uso">Termos de uso</a></li>
+            <li class=""><a href="<?php echo site_url() ?>/quem-faz/" title="Quem Faz">Quem Faz</a></li>
         </ul>
 
         <p class="text"><a href="http://www.mc.gov.br/">Ministério das Comunicações</a> - Alguns direitos reservados.</p>
  
         <p class="links">
           <a href="http://www.w3.org/" title="www.w3.org">
-            <img src="<?php bloginfo('template_url');?>/imgs/graph_w3c.png" alt="www.w3.org" />
+            <img src="<?php bloginfo('template_url');?>/global/img/graph/graph_w3c.png" alt="www.w3.org" />
           </a> 
           <a href="http://creativecommons.org/licenses/by-nc-sa/2.5/br/" title="www.creativecommons.org">
-            <img src="<?php bloginfo('template_url');?>/imgs/graph_cc.png" alt="www.creativecommons.org" />
+            <img src="<?php bloginfo('template_url');?>/global/img/graph/graph_cc.png" alt="www.creativecommons.org" />
          </a>
          <a href="http://www.wordpress.org/" title="www.wordpress.org">
-           <img src="<?php bloginfo('template_url');?>/imgs/graph_wp.png" alt="www.wordpress.org" />
+           <img src="<?php bloginfo('template_url');?>/global/img/graph/graph_wp.png" alt="www.wordpress.org" />
          </a>
         </p>
         <div class="clear"></div>
