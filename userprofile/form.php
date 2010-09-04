6f2e1171c8de8889d28d0be060ec48fca2eeff60
panico <panico@panico-laptop.(none)>
2010-09-03 21:57:14 -0300

    Foi adicionado a pasta userprofile

---
+++ userprofile/form.php
@@ -0,0 +1,11 @@
+<script language="javascript">
+
+  function abrePop(){
+    window.open("./wp-content/themes/formacaogesac_bp/userprofile/ponto.php",'popup','menubar=no,scrollbars=yes,scrolling=no,adressbar=no,statsbar=no,width=800,height=420,top=50,left=50')
+  }
+</script>
+
+<div class="customField">
+    <a href="javascript:;" onclick="javascript:abrePop();">Consulte o ponto que você pertence</a>
+  </label>
+</div>
