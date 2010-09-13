559190b959bbfc0767bf9f7c09effbcf823c566a
Estela <estela@juggernaut.(none)>
2010-09-13 19:50:23 -0300

    inclusão do arquivo termo.php para abrir o termo de uso em nova janela e registro do termo em functions.php

---
+++ userprofile/termo.php
@@ -0,0 +1,11 @@
+<script language="javascript">
+
+  function abreTermo(){
+    window.open("http://gesac.ifce.edu.br/wp/termo-de-uso/",'popup','menubar=no,scrollbars=yes,scrolling=no,adressbar=no,statsbar=no,width=800,height=420,top=50,left=50')
+  }
+</script>
+
+<div class="customField">
+    Para acessar o termo de uso completo, <a href="javascript:;" onclick="javascript:abreTermo();"> clique aqui</a>!
+  </label>
+</div>
