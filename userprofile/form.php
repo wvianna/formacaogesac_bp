2471bff79f14e066cf6c33c0c12a80de0de55555
Filipe Ribeiro <root@panico-laptop.(none)>
2010-09-16 18:19:10 -0300

    Alterado o endereço do pop-up e o tamanho

--- userprofile/form.php
+++ userprofile/form.php
@@ -1,7 +1,7 @@
 <script language="javascript">
 
   function abrePop(){
-    window.open("./wp-content/themes/formacaogesac_bp/userprofile/ponto.php",'popup','menubar=no,scrollbars=yes,scrolling=no,adressbar=no,statsbar=no,width=800,height=420,top=50,left=50')
+    window.open("./wp-content/themes/formacaogesac_bp/pontoscadastro.php",'popup','menubar=no,scrollbars=yes,scrolling=no,adressbar=no,statsbar=no,width=1100,height=420,top=50,left=50')
   }
 </script>
 
