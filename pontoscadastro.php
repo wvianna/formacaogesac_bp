d24d291ba58f6857fc1bc11ba351aa9b9bcf4f55
Filipe Ribeiro <root@panico-laptop.(none)>
2010-09-16 18:09:47 -0300

    Acrescentado a página para consulta dos pontos do momento do cadastro

---
+++ pontoscadastro.php
@@ -0,0 +1,84 @@
+<?php
+/** Loads the WordPress Environment and Template */
+require('../../../wp-blog-header.php');
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
+	<head>
+        <title><?php bp_page_title() ?></title>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+   		<?php do_action( 'bp_head' ) ?>
+        <link rel="icon" href="<?php bloginfo('stylesheet_directory'); ?>/favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="<?php bloginfo('stylesheet_directory'); ?>/favicon.ico" type="image/x-icon" />
+        <?php if ( function_exists( 'bp_sitewide_activity_feed_link' ) ) : ?>
+			<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> | <?php _e('Site Wide Activity RSS Feed', 'buddypress' ) ?>" href="<?php bp_sitewide_activity_feed_link() ?>" />
+		<?php endif; ?>
+		<?php if ( function_exists( 'bp_member_activity_feed_link' ) && bp_is_member() ) : ?>
+			<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> | <?php bp_displayed_user_fullname() ?> | <?php _e( 'Activity RSS Feed', 'buddypress' ) ?>" href="<?php bp_member_activity_feed_link() ?>" />
+		<?php endif; ?>
+		<?php if ( function_exists( 'bp_group_activity_feed_link' ) && bp_is_group() ) : ?>
+			<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> | <?php bp_current_group_name() ?> | <?php _e( 'Group Activity RSS Feed', 'buddypress' ) ?>" href="<?php bp_group_activity_feed_link() ?>" />
+		<?php endif; ?>
+		<link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> <?php _e( 'Blog Posts RSS Feed', 'buddypress' ) ?>" href="<?php bloginfo('rss2_url'); ?>" />
+		<link rel="alternate" type="application/atom+xml" title="<?php bloginfo('name'); ?> <?php _e( 'Blog Posts Atom Feed', 'buddypress' ) ?>" href="<?php bloginfo('atom_url'); ?>" />
+		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+        <!--[if IE]>
+			<script type='text/javascript' src='http://getfirebug.com/releases/lite/1.2/firebug-lite-compressed.js'></script>
+			<link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/global/css/ie.css" />
+        <![endif]-->
+        <?php wp_head(); ?>
+    
+
+
+  <div id="mainContent" class="container_11">
+    <div id="content" class="grid_900">
+    <div id="pageContent" class="main">
+
+<h1>Pontos atendidos</h1>
+<?php
+echo'  
+  <label>
+    Estado
+  </label>
+  <form action="pontoscadastroresult.php" method="post">
+   <select name="estado" id="estado">  
+    <option value="">----</option>1
+    <option value="AC">Acre</option>
+    <option value="AL">Alagoas</option>
+    <option value="AP">Amapá</option>
+    <option value="AM">Amazonas</option>
+    <option value="BA">Bahia</option>
+    <option value="CE">Ceará</option>
+    <option value="DF">Distrito Federal</option>
+    <option value="ES">Espírito Santo</option>
+    <option value="GO">Goiás</option>
+    <option value="MA">Maranhão</option>
+    <option value="MT">Mato Grosso</option>
+    <option value="MS">Mato Grosso do Sul</option>
+    <option value="MG">Minas Gerais</option>
+    <option value="PA">Pará</option>
+    <option value="PB">Paraíba</option>
+    <option value="PR">Paraná</option>
+    <option value="PE">Pernambuco</option>
+    <option value="PI">Piauí</option>
+    <option value="RJ">Rio de Janeiro</option>
+    <option value="RN">Rio Grande do Norte</option>
+    <option value="RS">Rio Grande do Sul</option>
+    <option value="RO">Rondônia</option>
+    <option value="RR">Roraima</option>
+    <option value="SC">Santa Catarina</option>
+    <option value="SP">São Paulo</option>
+    <option value="SE">Sergipe</option>
+    <option value="TO">Tocantins</option>
+   </select>
+   <INPUT type="submit" value="Enviar"> 
+  </form>';
+?>
+                    
+   
+    </div>
+    </div>
+    <div class="clear"></div>
+</div>
