e8bb42a6a668fedfa86086a0b2422bd68d77e3fb
Equipe NSI IFF Campos <fgesac@gmail.com>
2010-09-23 20:04:07 -0300

    alterações no CSS

--- userprofile/termo.php
+++ userprofile/termo.php
@@ -5,6 +5,27 @@
   }
 </script>
 
+<textarea rows="10" cols="80" style="width:50%;text-align:" readonly="readonly" onfocus="this.rows=15">﻿RESUMO TERMO DE USO REDE GESAC
+
+     Por favor, leia este o Termo de Uso completo cuidadosamente antes de acessar ou usar o site. Ao usar ou acessar qualquer parte deste site, você concorda que está sujeito a estes termos e condições. Se você não concorda, você não deve acessar o site. Abaixo, disponibilizamos alguns itens importantes do Termo: 
+
+     - Ao criar seu perfil no site, você é responsável por manter a segurança de sua conta e de sua página, assim com é totalmente responsável por toda atividade da sua conta e outras ações conexas com seu perfil e páginas. Você não deve descrever ou criar palavras-chave para sua página de maneira enganosa ou ilegal, incluindo de maneira a insultar o nome ou reputação de outros;
+
+     - Não realize o download, a cópia e o uso de conteúdos que infringem direitos de propriedade, incluindo, mas não se limitando ao copyright, patente, marcas de qualquer terceira parte;
+
+     - Não publique conteúdos pornográficos, difamatórios ou caluniosos, que contém ameaças ou possam incitar violência contra indivíduos ou entidades, e não viole a privacidade ou direitos de publicidade de terceiros; 
+
+     - Verifique se página não é divulgada por meio de mensagens eletrônicas indesejáveis como spam em grupos, listas de e-mails, outras páginas ou métodos similares de promoção não-solicitada;
+
+     - Verifique se seu perfil e página não foram intitulados de maneira a levar os leitores a   acreditar que você é outra pessoa ou entidade;
+
+     - Ao submeter conteúdos para publicação via Rede Gesac, você confere ao site licença ampla, irrestrita, não-exclusiva e sem remuneração para reprodução, modificação, adaptação e publicação do material com o único propósito de mostrar, distribuir e promover sua página;
+
+     - O Rede Gesac se reserva o direito de, discricionariamente, modificar ou substituir qualquer parte deste acordo. É sua responsabilidade verificar este acordo periodicamente. O uso continuado do Rede Gesac após mudanças nos termos deste documento implica a aceitação dessas mudanças;
+
+Leia atentamente o Termo de Uso completo.
+</textarea>
+
 <div class="customField">
     Para acessar o termo de uso completo, <a href="javascript:;" onclick="javascript:abreTermo();"> clique aqui</a>!
   </label>
