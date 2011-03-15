a4402bde6155f4bdf08b75b61e9c5898617efd98
Estela <estela@juggernaut.(none)>
2011-03-15 15:17:35 -0300

    alteração no arquivo permalink.php para corrigir a inserção de respostas na rede social

--- members/single/activity/permalink.php
+++ members/single/activity/permalink.php
@@ -57,47 +57,65 @@
                             </div>
 
                             <?php if ( 'activity_comment' == bp_get_activity_type() ) : ?>
-                                <div class="activity-inreplyto">
-                                    <strong><?php _e( 'In reply to', 'buddypress' ) ?></strong> - <?php bp_activity_parent_content() ?> &middot;
-                                    <a href="<?php bp_activity_thread_permalink() ?>" class="view" title="<?php _e( 'View Thread / Permalink', 'buddypress' ) ?>"><?php _e( 'View', 'buddypress' ) ?></a>
-                                </div>
-                            <?php endif; ?>
+				<div class="activity-inreplyto">
+					<strong><?php _e( 'In reply to', 'buddypress' ) ?></strong> - 
+					<?php bp_activity_parent_content() ?> &middot;
+					<a href="<?php bp_activity_thread_permalink() ?>" 
+					   class="view"
+                                           title="<?php _e( 'View Thread / Permalink', 'buddypress' ) ?>">
+						<?php _e( 'View', 'buddypress' ) ?>
+					</a>
+				 </div>
+			     <?php endif; ?>
+                             <?php do_action( 'bp_before_activity_entry_comments' ) ?>
+	                     <?php if ( bp_activity_can_comment() ) : ?>
+		                  <div class="activity-comments">
+					<?php bp_activity_comments() ?>
+					<?php if ( is_user_logged_in() ) : ?>
+						<form action="<?php bp_activity_comment_form_action() ?>" 
+                                                      method="post"
+                                                      id="ac-form-<?php bp_activity_id() ?>"
+                                                      class="ac-form"<?php bp_activity_comment_form_nojs_display() ?>>
+	                                                <div class="ac-reply-avatar">
+								<?php bp_loggedin_user_avatar( 'width=25&height=25' ) ?>
+                                                        </div>
+							<div class="ac-reply-content">
+								<div class="ac-textarea">
+									<textarea id="ac-input-<?php bp_activity_id() ?>" 
+	                                                                          rows="7"
+ 	                                                                          cols="7"
+         	                                                                  class="ac-input" 
+                 	                                                          name="ac_input_<?php bp_activity_id() ?>">
+                         	                                        </textarea>
+								</div>
+							    	<input type="submit" 
+                                                                   name="ac_form_submit" 
+                                                                   class="submitDefault" 
+                                                                   value="<?php _e( 'Post', 'buddypress' ) ?> &rarr;" />  
+                                                            	&nbsp;
+                                                            	<?php _e( 'or press esc to cancel.', 'buddypress' ) ?>
+							   	<input type="hidden" 
+                                                                       name="comment_form_id"
+                                                                       value="<?php bp_activity_id() ?>" />
+							  </div>
+							  <?php echo str_replace( 'id=', 'class=', wp_nonce_field( 'new_activity_comment', '_wpnonce_new_activity_comment', true, false ) ); ?>
+						    </form>
+					<?php endif; ?>
+				</div>
+			   <?php endif; ?>
 
-                            <?php do_action( 'bp_before_activity_entry_comments' ) ?>
+			   <?php do_action( 'bp_after_activity_entry_comments' ) ?>
+		      </li>
+                      <?php do_action( 'bp_after_activity_entry' ) ?>
 
-                            <?php if ( bp_activity_can_comment() ) : ?>
-                                <div class="activity-comments">
-                                    <?php bp_activity_comments() ?>
+                  <?php endwhile; ?>
+              </ul>
+          <?php endif; ?>
+       </div>
+     </div>
+   </div>
 
-                                    <?php if ( is_user_logged_in() ) : ?>
-                                    <form action="<?php bp_activity_comment_form_action() ?>" method="post" id="ac-form-<?php bp_activity_id() ?>" class="ac-form"<?php bp_activity_comment_form_nojs_display() ?>>
-                                        <div class="ac-reply-avatar"><?php bp_loggedin_user_avatar( 'width=25&height=25' ) ?></div>
-                                        <div class="ac-reply-content">
-                                            <div class="ac-textarea">
-                                                <textarea id="ac-input-<?php bp_activity_id() ?>" rows="7" cols="7" class="ac-input" name="ac_input_<?php bp_activity_id() ?>"></textarea>
-                                            </div>
-                                            <input type="submit" name="ac_form_submit" class="submitDefault" value="<?php _e( 'Post', 'buddypress' ) ?> &rarr;" /> &nbsp; <?php _e( 'or press esc to cancel.', 'buddypress' ) ?>
-                                            <input type="hidden" name="comment_form_id" value="<?php bp_activity_id() ?>" />
-                                        </div>
-                                        <?php wp_nonce_field( 'new_activity_comment', '_wpnonce_new_activity_comment' ) ?>
-                                    </form>
-                                    <?php endif; ?>
-                                </div>
-                            <?php endif; ?>
-
-                            <?php do_action( 'bp_after_activity_entry_comments' ) ?>
-                        </li>
-
-                        <?php do_action( 'bp_after_activity_entry' ) ?>
-
-                    <?php endwhile; ?>
-                    </ul>
-                <?php endif; ?>
-            </div>
-    </div>
-    </div>
-
-  <div class="clear"></div>
-  </div>
+   <div class="clear"></div>
+</div>
             
 <?php get_footer() ?>
